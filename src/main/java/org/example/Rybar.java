8a8eab2e1f273e2b0ed32ab0ff4b45ae1e8a182f
matus <matus.kundracik@gmail.com>
2024-11-24 01:41:59 +0100

    Rybar class udaje SpravaRybara class

---
+++ src/main/java/org/example/Rybar.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import java.time.LocalDate;
+import lombok.Data;
+@Data
+public class Rybar {
+
+    private int rybarId;
+    private String meno;
+    private String priezvisko;
+    private LocalDate datumNarodenia;
+    private String adresa;
+    private String statnaPrislusnost;
+    private String cisloOP;
+    private int evidentneCislo;
+    private int clenomOd;
+    private LocalDate pridanyDoEvidencie;
+    private LocalDate odhlasenyZEvidencie;
+}
