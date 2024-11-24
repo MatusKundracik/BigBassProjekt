60f35d21d295e2c3220396c7b21fdb73051c800c
sachista-simon <simonks259@gmail.com>
2024-11-24 02:28:56 +0100

    Class Ulovok + class SpravaUlovku

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,24 @@ package org.example;
 
 import lombok.var;
 
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         var peto = new Rybar();
         peto.setMeno("Martin");
+
+        var kapor = new Ulovok();
+        kapor.setIdUlovok(001);
+        kapor.setDatumUlovku(LocalDate.of(2024,1,22));
+        kapor.setCisloReviru(40102);
+        kapor.setDruhRyby("kapor");
+        kapor.setDlzkaVcm(40);
+        kapor.setHmotnostVkg(20);
+
+        System.out.println(kapor);
 
     }
 }
