d5a4bd83d606dc2ba5ccc31ecc6910698174da77
matus <matus.kundracik@gmail.com>
2024-12-02 12:56:38 +0100

    UnitTest na SpravaRybara

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,25 +8,5 @@ import java.time.LocalDate;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        var peto = new Rybar();
-        var sp = new SpravaRybara();
-        peto.setRybarId(1111);
-        peto.setMeno("Martin");
-        peto.setPriezvisko("Novák");
-        peto.setDatumNarodenia(LocalDate.of(1990, 5, 15));
-        peto.setAdresa("Bratislava");
-        peto.setStatnaPrislusnost("SVK");
-        peto.setCisloOP("ID123456");
-        peto.setEvidentneCislo(1234);
-        peto.setClenomOd(2015);
-        peto.setPridanyDoEvidencie(LocalDate.of(2020,6,16));
-        peto.setOdhlasenyZEvidencie(null); // Je stále člen
-        sp.pridatRybara(peto);
-        sp.vypisRybarov();
-        sp.zmenitMeno(1111, "Matus");
-        sp.zmenitPriezvisko(1111,"Kundracik");
-        sp.vypisRybarov();
-        sp.odobratRybara(1111);
-
     }
 }
