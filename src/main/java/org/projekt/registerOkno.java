f0a21a0bd1efca1905c51fbbacf3fd3be34d0200
matus <matus.kundracik@gmail.com>
2024-12-03 01:23:17 +0100

    dá sa zapnuť okno Registrácie, pridal som okno zo scenebuilderu, okno nema žiadnu funcionalitu

---
+++ src/main/java/org/projekt/registerOkno.java
@@ -0,0 +1,25 @@
+package org.projekt;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class registerOkno extends Application {
+
+    public void start(Stage stage) throws Exception {
+        Parent rootPane = FXMLLoader.load(getClass().getResource("/RegisterController.fxml"));
+
+        Scene scene = new Scene(rootPane);
+        stage.setTitle("Register");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
