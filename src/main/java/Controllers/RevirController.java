070b5034b5ac7a4e07404a270c18c1dc3764112d
sachista-simon <simonks259@gmail.com>
2024-12-10 00:50:06 +0100

    pridanie RevirControlleru

--- src/main/java/Controllers/RevirController.java
+++ src/main/java/Controllers/RevirController.java
@@ -3,9 +3,16 @@ package Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 public class RevirController {
+
+    @FXML
+    private CheckBox kaproveCheckBox;
+
+    @FXML
+    private CheckBox lipnoveCheckBox;
 
     @FXML
     private TextField lokalitaTextField;
@@ -17,10 +24,29 @@ public class RevirController {
     private TextField popisTextField;
 
     @FXML
+    private CheckBox pstruhoveCheckBox;
+
+    @FXML
     private Button zapisRevirButton;
 
     @FXML
     void addRevirButton(ActionEvent event) {
 
     }
+
+    @FXML
+    void kaproveAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lipnoveAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pstruhoveAction(ActionEvent event) {
+
+    }
+
 }
