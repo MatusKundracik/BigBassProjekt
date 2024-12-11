f01e18b9b8e34a440ede22664caa7b6c22d1e208
sachista-simon <simonks259@gmail.com>
2024-12-12 00:45:28 +0100

    Rybar si môže pridať ulovok len do tých revirov na ktore ma povolenie

--- src/main/java/Controllers/UlovkyController.java
+++ src/main/java/Controllers/UlovkyController.java
@@ -110,17 +110,25 @@ public class UlovkyController {
     @FXML
     private void naplnNazvyReviruDoChoiceBox() {
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:bigbass.db")) {
-            String selectQuery = "SELECT id_revira, nazov FROM revir";
-            try (PreparedStatement statement = connection.prepareStatement(selectQuery);
-                 ResultSet resultSet = statement.executeQuery()) {
+            String selectQuery =  "SELECT r.nazov FROM revir r " +
+                    "JOIN povolenie p ON (" +
+                    "    (p.kaprové = 1 AND r.kaprove = 1) OR " +
+                    "    (p.pstruhove = 1 AND r.pstruhove = 1) OR " +
+                    "    (p.lipňove = 1 AND r.lipnove = 1)" +
+                    ") " +
+                    "WHERE p.rybar_id_rybara = ?";
+            try (PreparedStatement statement = connection.prepareStatement(selectQuery)) {
+                statement.setInt(1, Session.aktualnyRybarId);
 
-                nazovReviruComboBox.getItems().clear();
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    // Clear the existing items in the ChoiceBox
+                    nazovReviruComboBox.getItems().clear();
 
-                while (resultSet.next()) {
-                    String nazovReviru = resultSet.getString("nazov");
-                    int idRevir = resultSet.getInt("id_revira");
-                    revirMap.put(nazovReviru, idRevir);
-                    nazovReviruComboBox.getItems().add(nazovReviru);
+                    // Add each "nazov" from the database to the ChoiceBox
+                    while (resultSet.next()) {
+                        String nazovReviru = resultSet.getString("nazov");
+                        nazovReviruComboBox.getItems().add(nazovReviru);
+                    }
                 }
             }
         } catch (SQLException e) {
